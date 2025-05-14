bed849ec735e2c6ebd30f3ad17dc8e26a09ac543
Donna Bui <109771688+DonnaBui@users.noreply.github.com>
2025-05-14 10:42:25 -0700

    Add files via upload

---
+++ mvcApps/src/smartbox/components/Stack.java
@@ -0,0 +1,33 @@
+package smartbox.components;
+
+import smartbox.*;
+
+public class Stack extends Component implements IStack {
+
+	private java.util.Stack<Double> stack;
+
+	public Stack() {
+		super();
+		stack = new java.util.Stack<Double>();
+	}
+
+    public void push(Double num) {
+        stack.push(num);
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public Double top() {
+ 		return stack.peek();
+    }
+
+    public void clear() {
+        stack.clear();
+    }
+
+    public Boolean isEmpty() {
+        return stack.isEmpty();
+    }
+}
